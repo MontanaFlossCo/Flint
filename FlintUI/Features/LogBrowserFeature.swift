ed12888bee151ecc8fc32f61619a06a01535e785
Marc Palmer <marc@anyware.co.uk>
2018-05-01 14:56:38 +0100

    Update FlintUI to new contraints mech

--- FlintUI/Features/LogBrowserFeature.swift
+++ FlintUI/Features/LogBrowserFeature.swift
@@ -15,6 +15,7 @@ final public class LogBrowserFeature: ConditionalFeature {
     public static var description: String = "UI for browsing the Focus logs"
 
     public static func constraints(requirements: FeatureConstraintsBuilder) {
+        requirements.precondition(.platform(id: .iOS, version: .any))
         requirements.precondition(.runtimeEnabled)
     }
 
