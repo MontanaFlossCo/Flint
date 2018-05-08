7d5581f8e2cf643e45e84a31cf34567acf4235b3
Marc Palmer <marc@anyware.co.uk>
2018-05-08 17:27:46 +0100

    WIP

--- FlintCore/Conditional Features/ConditionalFeature.swift
+++ FlintCore/Conditional Features/ConditionalFeature.swift
@@ -44,6 +44,19 @@ public protocol ConditionalFeature: ConditionalFeatureDefinition {
     static func request<T>(_ actionBinding: ConditionalActionBinding<Self, T>) -> ConditionalActionRequest<Self, T>?
 }
 
+public enum SystemPermissionRequestAction {
+    case requestPermission
+    case skipPermission
+    case cancelAll
+}
+
+public protocol PermissionAuthorisationCoordinator {
+    func begin(for permissions: Set<SystemPermission>, completion: (_ permissionsToRequest: [SystemPermission]?) -> ())
+    func beforePermissionRequest(for permission: SystemPermission, completion: (_ action: SystemPermissionRequestAction) -> ())
+    func afterPermissionRequest(for permission: SystemPermission, status: SystemPermissionStatus)
+    func complete(for controller: AuthorisationController, cancelled: Bool)
+}
+
 public extension ConditionalFeature {
     
     /// Verifies that the feature is correctly prepared in Flint and tests `isAvailable` to see if it is true.
@@ -61,6 +74,26 @@ public extension ConditionalFeature {
         return ConditionalActionRequest(actionBinding: actionBinding)
     }
 
+    /// Request permissions for all unauthorised permission requirements, using the supplied presenter
+    public static func requestMissingPermissions(using coordinator: PermissionAuthorisationCoordinator,
+                                                 completion: (_ permission: SystemPermission, _ status: SystemPermissionStatus) -> Void) -> AuthorisationController {
+        let authorisationController = DefaultAuthorisationController(coordinator: coordinator)
+
+        let constraints = Flint.constraintsEvaluator.evaluate(for: self)
+        guard constraints.unsatisfied.permissions.count > 0 else {
+            return authorisationController
+        }
+        
+        let permissions = constraints.unsatisfied.permissions
+        
+        coordinator.begin(for: permissions) { permissionsToRequest in
+            if let orderedPermissions = permissionsToRequest, permissions.count > 0 {
+                authorisationController.begin(with: orderedPermissions)
+            }
+        }
+        return authorisationController
+    }
+    
     /// Function for binding a conditional feature and action pair, to restrict how this can be done externally by app code.
     public static func action<A>(_ action: A.Type) -> ConditionalActionBinding<Self, A> where A: Action {
         return ConditionalActionBinding(feature: self, action: action)
@@ -68,3 +101,49 @@ public extension ConditionalFeature {
 
 }
 
+public protocol AuthorisationController {
+    var permissions: [SystemPermission] { get }
+    
+    func cancel()
+}
+
+class DefaultAuthorisationController: AuthorisationController {
+    public var permissions: [SystemPermission] = []
+    var remainingPermissions: [SystemPermission] = []
+    let coordinator: PermissionAuthorisationCoordinator
+    
+    init(coordinator: PermissionAuthorisationCoordinator) {
+        self.coordinator = coordinator
+    }
+    
+    func begin(with permissions: [SystemPermission]) {
+        self.permissions = permissions
+        remainingPermissions = permissions
+        
+        next()
+    }
+    
+    func next() {
+        if let permission = remainingPermissions.first {
+            coordinator.beforePermissionRequest(for: permission) { action in
+                switch action {
+                    case .requestPermission:
+                        Flint.permissionChecker.requestAuthorization(for: permission) { [weak self] permission, status in
+                            self?.coordinator.afterPermissionRequest(for: permission, status: status)
+                        }
+                    case .skipPermission:
+                        next()
+                    case .cancelAll:
+                        remainingPermissions.removeAll()
+                        cancel()
+                }
+            }
+        } else {
+            coordinator.complete(for: self, cancelled: false)
+        }
+    }
+
+    public func cancel() {
+        coordinator.complete(for: self, cancelled: true)
+    }
+}
