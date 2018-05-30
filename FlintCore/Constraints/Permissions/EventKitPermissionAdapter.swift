9e29dcf45fd21f8157ae6d4336f606ffe0bc0b8c
Marc Palmer <marc@anyware.co.uk>
2018-05-30 20:47:51 +0100

    WIP

---
+++ FlintCore/Constraints/Permissions/EventKitPermissionAdapter.swift
@@ -0,0 +1,9 @@
+//
+//  EventKitPermissionAdapter.swift
+//  FlintCore
+//
+//  Created by Marc Palmer on 30/05/2018.
+//  Copyright © 2018 Montana Floss Co. Ltd. All rights reserved.
+//
+
+import Foundation
