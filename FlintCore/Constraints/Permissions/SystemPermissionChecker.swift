aca91da052790cf36337520684ae77c0b9c2d923
Marc Palmer <marc@anyware.co.uk>
2018-05-30 15:06:33 +0100

    Refactor permission checker to have mutable adapters lazily created, threadsafe

--- FlintCore/Constraints/Permissions/SystemPermissionChecker.swift
+++ FlintCore/Constraints/Permissions/SystemPermissionChecker.swift
@@ -14,6 +14,8 @@ public protocol SystemPermissionCheckerDelegate: AnyObject {
 
 /// The interface for the component that will check that required permissions are granted.
 ///
+/// Implementations must be safe to call from any thread.
+///
 /// - see: `DefaultPermissionChecker`
 public protocol SystemPermissionChecker {
     var delegate: SystemPermissionCheckerDelegate? { get set }
@@ -21,7 +23,7 @@ public protocol SystemPermissionChecker {
     /// Must return `true` only if all the permissions are authorised
     func isAuthorised(for permissions: Set<SystemPermissionConstraint>) -> Bool
 
-    /// Must return the status of the permission
+    /// Must return the status of the permission.
     func status(of permission: SystemPermissionConstraint) -> SystemPermissionStatus
 
     /// Must ask the user to grant the given permission
