84376b5c6195a2f07ceaa23b38f295f3f2bd2df3
Marc Palmer <marc@anyware.co.uk>
2018-06-14 11:42:02 +0100

    New metadata builder mechanism

--- FlintCore/Actions/MetadataEncodable.swift
+++ FlintCore/Actions/MetadataEncodable.swift
@@ -12,28 +12,55 @@ import CoreSpotlight
 #endif
 
 public struct Metadata {
-    public let title: String?
-    public let subtitle: String?
+    public private(set) var title: String?
+    public private(set) var subtitle: String?
 
 #if canImport(CoreSpotlight)
-#if canImport(Cocoa)
     /// Set to a thumbnail to show when displaying this activity
-    public let thumbnail: NSImage?
-#elseif canImport(UIKit)
-    /// Set to a thumbnail to show when displaying this activity
-    public let thumbnail: UIImage?
-#endif
+    public private(set) var thumbnail: FlintImage?
     /// Set to thumbnail data to show when displaying this activity
-    public let thumbnailData: Data?
+    public private(set) var thumbnailData: Data?
     /// Set to URL pointing at local thumbnail data to show when displaying this activity
-    public let thumbnailURL: URL?
+    public private(set) var thumbnailURL: URL?
 #endif
 
-    public let keywords: Set<String>?
+    public private(set) var keywords: Set<String>?
 
 #if canImport(CoreSpotlight) && (os(iOS) || os(macOS))
-    public let searchAttributes: CSSearchableItemAttributeSet?
+    public private(set) var searchAttributes: CSSearchableItemAttributeSet?
 #endif
+
+    public static func build(_ block: (_ builder: MetadataBuilder) -> ()) -> Metadata {
+        let builder = MetadataBuilder()
+        return builder.build(block)
+    }
+}
+
+public class MetadataBuilder {
+    public private(set) var title: String?
+    public private(set) var subtitle: String?
+
+#if canImport(CoreSpotlight)
+    /// Set to a thumbnail to show when displaying this activity
+    public private(set) var thumbnail: FlintImage?
+    /// Set to thumbnail data to show when displaying this activity
+    public private(set) var thumbnailData: Data?
+    /// Set to URL pointing at local thumbnail data to show when displaying this activity
+    public private(set) var thumbnailURL: URL?
+#endif
+
+    public private(set) var keywords: Set<String>?
+
+#if canImport(CoreSpotlight) && (os(iOS) || os(macOS))
+    public private(set) var searchAttributes: CSSearchableItemAttributeSet?
+#endif
+    
+    private var metadata = Metadata()
+    
+    func build(_ block: (_ builder: MetadataBuilder) -> ()) -> Metadata {
+        block(self)
+        return metadata
+    }
 }
 
 /// Allow inputs to provide information for user-facing activities and shortcuts
