c04329cc1b06a0c33770fc15751cfbe4ef585464
Marc Palmer <marc@anyware.co.uk>
2018-07-18 12:47:12 +0100

    Minor renaming and docs improvements

--- FlintCore/Routes/RouteParametersCodable.swift
+++ FlintCore/Routes/RouteParametersCodable.swift
@@ -8,20 +8,10 @@
 
 import Foundation
 
-/// !!! TODO: These need to change to URLRepresentable or similar, so that custom path and query param encodings can be
-/// applied based on whether an action is being encoded to a custom app scheme or a deep universal link e.g.:
-///
-/// x-app://open/?id=34534534
-///
-/// this is fine but the same link to share via e.g...
-///
-/// www.my-x-app.com/user/documents/open/34534534
-///
-/// That is more the kind of thing we want, where we take the prefix and add different path elements and maybe encode the
-/// other parts of the input state differently because it is a public URL.
-
+/// The type for parameters that can be decoded and encoded for a URL.
 public typealias RouteParameters = [String:String]
 
+/// The protocol to adopt to support creating an input type from an incoming URL that matched the route
 public protocol RouteParametersDecodable {
     init?(from routeParameters: RouteParameters?, mapping: URLMapping)
 }
--- FlintCore/Routes/URLMapped.swift
+++ FlintCore/Routes/URLMapped.swift
@@ -9,25 +9,46 @@
 import Foundation
 
 /// Features must adopt this protocol if they support URL mappings, and define
-/// the mappings from URLs to actions.
+/// the routes that map from URLs to actions.
 ///
-/// There is support for multiple custom app URL schemes and multiple associated domains.
+/// There is support for multiple custom app URL schemes and multiple associated domains, URL wildcards and named
+/// variables in the path components.
 public protocol URLMapped {
 
-    /// Call to initialise `urlMappings` using the routes DSL. You pass a closure that receives the builder instance,
-    /// and call functions on the builder to define how incoming URLs route to your feature's actions.
+    /// Called by Flint to initialise `urlMappings` using the routes builder. Your function receives the
+    /// builder instance and must call functions on the builder to define how incoming URLs route to your feature's actions.
+    ///
+    /// URL routes support basic wildcards and paths containing named parameters that are extracted as values passed to
+    /// your action input.
+    ///
+    /// Any query parameters from the URL are extracted and combined with named path parameters
+    /// you define in mappings, and passed to your action input type that must conform to `RouteParametersCodable`
     ///
     /// Example:
     ///
     /// ```
     /// static func urlMappings(routes: URLMappingsBuilder) {
+    ///     // "/create" for any source will go to createNew
     ///     routes.send("create", to: createNew)
-    ///     routes.send("open", to: openDocument)
+    ///
+    ///     // "x-legacy://open-doc" will go to openDocument
+    ///     routes.send("open-doc", to: openDocument, in: [.app(scheme: "x-legacy"])
+    ///
+    ///     // "https://yourapp.com/open" and any custom scheme for "/open"  will go to openDocument
+    ///     routes.send("open", to: openDocument, in: [.appAny, .universal(domain: "yourapp.com")])
+    ///
+    ///     // Match "/profile/" with any other path components after it e.g. "/profile/avatar" on any source will go to showProfile
+    ///     routes.send("profile/**", to: showProfile)
+    ///
+    ///     // Match "/blog/*/*/*/post-name-here" with any year/month/day values, extracting the post name ("name-here") as a route parameter passed
+    ///     to the input, routing to the action showPost
+    ///     routes.send("blog/*/*/*/post-$(postname)", to: showPost)
     /// }
     /// ```
+    /// - param routes: A builder that you use to define the URL routes supported by your feature.
     ///
-    /// - see: `URLMappingsBuilder` which supports routing to specific scopes and multiple scopes
-    static func urlMappings(routes routeBuilder: URLMappingsBuilder)
+    /// - see: `URLMappingsBuilder` for full details
+    static func urlMappings(routes: URLMappingsBuilder)
     
 }
 
