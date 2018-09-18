c40f6142aa6c58bd2bc5532fa69095115b9808cb
Marc Palmer <marc@anyware.co.uk>
2018-09-18 11:55:41 +0100

    Move helper funcs into feature definition. Fixes #204 (#205)

--- FlintCore/Conditional Features/ConditionalFeature.swift
+++ FlintCore/Conditional Features/ConditionalFeature.swift
@@ -39,8 +39,11 @@ import Foundation
 public protocol ConditionalFeature: ConditionalFeatureDefinition {
 
     /// Call to request invocation of the conditionally available action.
+    ///
     /// - return: nil if the action's feature is not available, or a request instance that can be used to `perform`
     /// the action directly or on a specific `ActionSession` if the feature is available
+    ///
+    /// - note: This is defined in the protocol so you may override it if you have other ways to validate features.
     static func request<T>(_ actionBinding: ConditionalActionBinding<Self, T>) -> ConditionalActionRequest<Self, T>?
 }
 
@@ -79,87 +82,11 @@ public extension ConditionalFeature {
         return ConditionalActionRequest(actionBinding: actionBinding)
     }
 
-    /// Returns a set of new context-specific loggers with this feature as the context (topic path).
-    ///
-    /// - param activity: A string that identifies the kind of activity that will be generating log entries, e.g. "bg upload"
-    /// - return: A `Logs` value which contains development and production loggers as appropriate at runtime.
-    public static func logs(for activity: String) -> ContextualLoggers {
-        let development: ContextSpecificLogger?
-        if let factory = Logging.development {
-            development = factory.contextualLogger(with: activity, topicPath: self.identifier)
-        } else {
-            development = nil
-        }
-
-        let production: ContextSpecificLogger?
-        if let factory = Logging.development {
-            production = factory.contextualLogger(with: activity, topicPath: self.identifier)
-        } else {
-            production = nil
-        }
-
-        return ContextualLoggers(development: development, production: production)
-    }
-    
-    /// Access information about the permissions required by this feature
-    public static var permissions: FeaturePermissionRequirements {
-        let constraints = Flint.constraintsEvaluator.evaluate(for: self)
-        
-        func _filter(_ permissions: [SystemPermissionConstraint], onStatus matchingStatus: SystemPermissionStatus) -> Set<SystemPermissionConstraint> {
-            let results = permissions.filter { permission in
-                let status = Flint.permissionChecker.status(of: permission)
-                return matchingStatus == status
-            }
-            return Set(results)
-        }
-        
-        let notDetermined = _filter(constraints.permissions.notDetermined.map { $0.constraint }, onStatus: .notDetermined)
-        let denied = _filter(constraints.permissions.notSatisfied.map { $0.constraint }, onStatus: .denied)
-        let restricted = _filter(constraints.permissions.notSatisfied.map { $0.constraint }, onStatus: .restricted)
-
-        return FeaturePermissionRequirements(all: Set(constraints.permissions.all.map { $0.constraint }),
-                                             notDetermined: notDetermined,
-                                             denied: denied,
-                                             restricted: restricted)
-    }
-    
-    /// Access information about the purchases required by this feature
-    public static var purchases: FeaturePurchaseRequirements {
-        // Ugly implementation of this for now until we patch up `FeatureConstraints` internals
-        func _extractPurchaseRequirements(_ preconditions: [FeaturePreconditionConstraint]) -> Set<PurchaseRequirement> {
-            let requirements: [PurchaseRequirement] = preconditions.compactMap {
-                if case let .purchase(requirement) = $0 {
-                    return requirement
-                } else {
-                    return nil
-                }
-            }
-            return Set(requirements)
-        }
-    
-        let constraints = Flint.constraintsEvaluator.evaluate(for: self)
-        let all = _extractPurchaseRequirements(constraints.preconditions.all.map { $0.constraint })
-        let requiredToUnlock = _extractPurchaseRequirements(constraints.preconditions.notSatisfied.map { $0.constraint })
-        let purchased = _extractPurchaseRequirements(constraints.preconditions.satisfied.map { $0.constraint })
-        
-        return FeaturePurchaseRequirements(all: all, requiredToUnlock: requiredToUnlock, purchased: purchased)
-    }
-    
-    /// Request permissions for all unauthorised permission requirements, using the supplied presenter
-    public static func permissionAuthorisationController(using coordinator: PermissionAuthorisationCoordinator?) -> AuthorisationController? {
-        let constraints = Flint.constraintsEvaluator.evaluate(for: self)
-        guard constraints.permissions.notDetermined.count > 0 else {
-            return nil
-        }
-        
-        return DefaultAuthorisationController(coordinator: coordinator,
-                                              permissions: Set(constraints.permissions.notDetermined.map { $0.constraint }))
-    }
-    
     /// Function for binding a conditional feature and action pair, to restrict how this can be done externally by app code.
+    ///
+    /// - note: This exists only in an extension as it is not an extension point for features to override.
     public static func action<A>(_ action: A.Type) -> ConditionalActionBinding<Self, A> where A: Action {
         return ConditionalActionBinding(feature: self, action: action)
     }
-
 }
 
--- FlintCore/Conditional Features/ConditionalFeatureDefinition.swift
+++ FlintCore/Conditional Features/ConditionalFeatureDefinition.swift
@@ -14,6 +14,10 @@ import Foundation
 /// This type exists separately from `ConditionalFeature` so that other types of conditional feature
 /// can exist (e.g. a future `ConditionalFeatureGroup`).
 ///
+/// We also have this type to enable us to reference conditional features without generic constraint issues
+/// that arise from the Self requirement of `ConditionalFeature`. This allows us to define helper functions
+/// that operate on conditional features without having to deal with those problems.
+///
 /// - note: Accesses to any properties that may change at runtime, e.g. `isAvailable` must only occur on the main thread.
 public protocol ConditionalFeatureDefinition: FeatureDefinition {
     /// Called to define the requirements of this feature
@@ -37,5 +41,83 @@ public extension ConditionalFeatureDefinition {
     /// - see: `AvailabilityChecker`
     static var isAvailable: Bool? {
         return Flint.availabilityChecker?.isAvailable(self)
+    }
+
+
+    /// Returns a set of new context-specific loggers with this feature as the context (topic path).
+    ///
+    /// - param activity: A string that identifies the kind of activity that will be generating log entries, e.g. "bg upload"
+    /// - return: A `Logs` value which contains development and production loggers as appropriate at runtime.
+    public static func logs(for activity: String) -> ContextualLoggers {
+        let development: ContextSpecificLogger?
+        if let factory = Logging.development {
+            development = factory.contextualLogger(with: activity, topicPath: self.identifier)
+        } else {
+            development = nil
+        }
+
+        let production: ContextSpecificLogger?
+        if let factory = Logging.development {
+            production = factory.contextualLogger(with: activity, topicPath: self.identifier)
+        } else {
+            production = nil
+        }
+
+        return ContextualLoggers(development: development, production: production)
+    }
+    
+    /// Access information about the permissions required by this feature
+    public static var permissions: FeaturePermissionRequirements {
+        let constraints = Flint.constraintsEvaluator.evaluate(for: self)
+        
+        func _filter(_ permissions: [SystemPermissionConstraint], onStatus matchingStatus: SystemPermissionStatus) -> Set<SystemPermissionConstraint> {
+            let results = permissions.filter { permission in
+                let status = Flint.permissionChecker.status(of: permission)
+                return matchingStatus == status
+            }
+            return Set(results)
+        }
+        
+        let notDetermined = _filter(constraints.permissions.notDetermined.map { $0.constraint }, onStatus: .notDetermined)
+        let denied = _filter(constraints.permissions.notSatisfied.map { $0.constraint }, onStatus: .denied)
+        let restricted = _filter(constraints.permissions.notSatisfied.map { $0.constraint }, onStatus: .restricted)
+
+        return FeaturePermissionRequirements(all: Set(constraints.permissions.all.map { $0.constraint }),
+                                             notDetermined: notDetermined,
+                                             denied: denied,
+                                             restricted: restricted)
+    }
+    
+    /// Access information about the purchases required by this feature
+    public static var purchases: FeaturePurchaseRequirements {
+        // Ugly implementation of this for now until we patch up `FeatureConstraints` internals
+        func _extractPurchaseRequirements(_ preconditions: [FeaturePreconditionConstraint]) -> Set<PurchaseRequirement> {
+            let requirements: [PurchaseRequirement] = preconditions.compactMap {
+                if case let .purchase(requirement) = $0 {
+                    return requirement
+                } else {
+                    return nil
+                }
+            }
+            return Set(requirements)
+        }
+    
+        let constraints = Flint.constraintsEvaluator.evaluate(for: self)
+        let all = _extractPurchaseRequirements(constraints.preconditions.all.map { $0.constraint })
+        let requiredToUnlock = _extractPurchaseRequirements(constraints.preconditions.notSatisfied.map { $0.constraint })
+        let purchased = _extractPurchaseRequirements(constraints.preconditions.satisfied.map { $0.constraint })
+        
+        return FeaturePurchaseRequirements(all: all, requiredToUnlock: requiredToUnlock, purchased: purchased)
+    }
+    
+    /// Request permissions for all unauthorised permission requirements, using the supplied presenter
+    public static func permissionAuthorisationController(using coordinator: PermissionAuthorisationCoordinator?) -> AuthorisationController? {
+        let constraints = Flint.constraintsEvaluator.evaluate(for: self)
+        guard constraints.permissions.notDetermined.count > 0 else {
+            return nil
+        }
+        
+        return DefaultAuthorisationController(coordinator: coordinator,
+                                              permissions: Set(constraints.permissions.notDetermined.map { $0.constraint }))
     }
 }
