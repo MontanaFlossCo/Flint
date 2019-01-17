f5e4b47a92e7812d2abf669fc5d412317d871652
Marc Palmer <marc@anyware.co.uk>
2019-01-17 15:35:23 +0000

    Doc fixup

--- FlintCore/Siri Intents and Shortcuts/StaticActionBinding+Shortcut+Extensions.swift
+++ FlintCore/Siri Intents and Shortcuts/StaticActionBinding+Shortcut+Extensions.swift
@@ -12,10 +12,13 @@ import Foundation
 #if canImport(Network) && os(iOS)
 extension StaticActionBinding {
 
-    /// Call to invoke the system "Add Voice Shortcut" view controller for the given input to the action represented.
-    /// by this action binding.
+    /// Call to invoke the system "Add Voice Shortcut" view controller for the given input to the conditionally-available
+    /// action represented by this action request. The action must support creating an `INIntent` for a custom intent extension
+    /// to be invoked, or creating an `NSUserActivity` using Flint's Activities conventions.
     ///
-    /// - note: Currently only actions that support NSUserActivity by opting in with `activityEligibility` are supported.
+    /// This will create a shortcut that invokes the `INIntent` returned by the `Action`'s `intent(for:)` function.
+    /// If that function returns nil (or is not defined by your `Action`), it will attempt to create an `NSUserActivity`
+    /// for the `Action` and instead use that. If the `Action` does not support `Activities`, this will fail.
     ///
     /// - param input: The input to pass to the action when it is later invoked from the Siri Shortcut by the user.
     /// - param presenter: The `UIViewController` to use to present the view controller
