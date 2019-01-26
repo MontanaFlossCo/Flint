9f1a2097af771ce6fa690797c08e759249af1034
Marc Palmer <marc@anyware.co.uk>
2019-01-26 14:03:49 +0000

    Missing documentation for IntentAction

--- FlintCore/Siri Intents and Shortcuts/IntentAction.swift
+++ FlintCore/Siri Intents and Shortcuts/IntentAction.swift
@@ -24,18 +24,28 @@ public protocol IntentBackgroundAction: Action {
 }
 
 #if canImport(Intents) && os(iOS)
+/// Adopt this protocol when implementing an action that fulfills a Siri Intent via an Intent Extension
 @available(iOS 12, *)
 public protocol IntentAction: IntentBackgroundAction {
+    /// The type of the `INIntent` thatt this action will implement. This is the Xcode-generated response type produced from your
+    /// intent definition configuration.
     associatedtype IntentType: FlintIntent
+    /// The type of the `INIntentResponse` for the `INIntent`. This is the Xcode-generated response type produced from your
+    /// intent definition configuration.
     associatedtype IntentResponseType: FlintIntentResponse
 
+    /// Automatic aliasing of the presenter to the appropriate type for Intents
     typealias PresenterType = IntentResponsePresenter<IntentResponseType>
 
     /// Implement this function if the Action supports a Siri Intent for Shortcuts. This is used to register
     /// a shortcut intent with Siri if you have the `IntentShortcutDonationFeature` enabled.
+    /// - param input: The input instance. Read properties of this to create an instance of the intent type.
     @available(iOS 12, *)
     static func intent(for input: InputType) -> IntentType?
 
+    /// Implement this function to create a valid input for the action from and instance of the `IntentType`, used
+    /// when performing the action for an intent.
+    /// - param intent: The intent instance. Read properties of this to create an instance of the input type.
     @available(iOS 12, *)
     static func input(for intent: IntentType) -> InputType?
 }
@@ -44,6 +54,9 @@ public protocol IntentAction: IntentBackgroundAction {
 /// Set up the queue and session to use for Siri actions because these cannot use the main queue.
 @available(iOS 12, *)
 public extension IntentBackgroundAction {
+    /// Defaults to the serial queue on which intent actions will be performed
     static var queue: DispatchQueue { return intentsQueue }
+    
+    /// Default session for Intent actions, so that they are all grouped easily
     static var defaultSession: ActionSession? { return intentActionSession }
 }
