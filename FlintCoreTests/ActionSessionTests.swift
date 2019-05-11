b4bdba8735c89a03729d7437d05a7b77aa02b72a
Marc Palmer <marc@anyware.co.uk>
2019-05-11 11:37:33 +0100

    Rewrite test for terseness

--- FlintCoreTests/ActionSessionTests.swift
+++ FlintCoreTests/ActionSessionTests.swift
@@ -18,178 +18,99 @@ class ActionSessionTests: XCTestCase {
         Flint.quickSetup(TestFeatures.self, domains: [], initialDebugLogLevel: .off, initialProductionLogLevel: .off)
     }
     
-    /// Verify that calling an action that must be called on the main queue, completes when called
-    /// from the main queue.
-    func testMainQueueSyncActionFromMainQueue() {
-        // Use the presenter to detect if the body of the action was actually called, to guard against the session
-        // giving us completion without invoking the action
-        let presenter = MockPresenter()
-        
-        let completionExpectation = expectation(description: "Sync completion called")
-        ActionSession.main.perform(TestFeatures.syncMainThreadAction,
-                                   input: .noInput,
-                                   presenter: presenter,
-                                   completion: { (outcome: ActionOutcome) in
-            XCTAssert(outcome == .success, "Action should have completed successfully")
-            completionExpectation.fulfill()
-        })
-
-        waitForExpectations(timeout: 5, handler: nil)
-        
-        XCTAssert(presenter.called, "Action was not performed")
-    }
-
-    /// Test that calling a main-queue action asynchronously on another queue works.
-    func testMainQueueSyncActionFromNonMainQueue() {
-        // Use the presenter to detect if the body of the action was actually called, to guard against the session
-        // giving us completion without invoking the action
-        let presenter = MockPresenter()
-        
-        let completionExpectation = expectation(description: "Sync completion called")
-        DispatchQueue.global(qos: .background).async {
-            ActionSession.main.perform(TestFeatures.syncMainThreadAction,
-                                       input: .noInput,
-                                       presenter: presenter,
-                                       completion: { (outcome: ActionOutcome) in
-                XCTAssert(outcome == .success, "Action should have completed successfully")
-                completionExpectation.fulfill()
-            })
-        }
-        
-        waitForExpectations(timeout: 5, handler: nil)
-        
-        XCTAssert(presenter.called, "Action was not performed")
-    }
-
-    /// Verify that calling an action that must be called on a background queue, completes when called
-    /// from the main queue.
-    func testBackgroundQueueSyncActionFromMainQueue() {
-        // Use the presenter to detect if the body of the action was actually called, to guard against the session
-        // giving us completion without invoking the action
-        let presenter = MockPresenter()
-        
-        let completionExpectation = expectation(description: "Sync completion called")
-        ActionSession.main.perform(TestFeatures.syncBackgroundThreadAction,
-                                           input: .noInput,
-                                           presenter: presenter,
-                                           completion: { (outcome: ActionOutcome) in
-            XCTAssert(outcome == .success, "Action should have completed successfully")
-            completionExpectation.fulfill()
-        })
-
-        waitForExpectations(timeout: 5, handler: nil)
-        
-        XCTAssert(presenter.called, "Action was not performed")
-    }
-
     /// Verify that calling an action that must be called on a background queue, completes when called
     /// from the correct background queue.
-    func testBackgroundQueueSyncActionFromBackgroundQueue() {
+    fileprivate func _testAction<ActionType>(_ actionBinding: StaticActionBinding<TestFeatures, ActionType>,
+            session: ActionSession? = ActionType.defaultSession,
+            asyncOnQueue queue: DispatchQueue? = nil)
+            where ActionType: Action, ActionType.InputType == NoInput, ActionType.PresenterType == MockPresenter {
         // Use the presenter to detect if the body of the action was actually called, to guard against the session
         // giving us completion without invoking the action
         let presenter = MockPresenter()
-        
         let completionExpectation = expectation(description: "Sync completion called")
-        SyncBackgroundThreadAction.queue.async {
-            Sessions.backgroundSession.perform(TestFeatures.syncBackgroundThreadAction,
-                                               input: .noInput,
-                                               presenter: presenter,
-                                               completion: { (outcome: ActionOutcome) in
+        
+        let block = {
+            (session ?? ActionSession.main).perform(actionBinding,
+                                                    input: .noInput,
+                                                    presenter: presenter,
+                                                    completion: { (outcome: ActionOutcome) in
                 XCTAssert(outcome == .success, "Action should have completed successfully")
                 completionExpectation.fulfill()
             })
         }
         
-        waitForExpectations(timeout: 5, handler: nil)
-        
-        XCTAssert(presenter.called, "Action was not performed")
-    }
- 
-    /// Verify that calling an action that must be called on the main queue and completes asynchronously does so when called
-    /// from the main queue.
-    func testMainQueueAsyncCompletingActionFromMainQueue() {
-        // Use the presenter to detect if the body of the action was actually called, to guard against the session
-        // giving us completion without invoking the action
-        let presenter = MockPresenter()
-        
-        let completionExpectation = expectation(description: "Async completion called")
-        ActionSession.main.perform(TestFeatures.asyncCompletingMainThreadAction,
-                                   input: .noInput,
-                                   presenter: presenter,
-                                   completion: { (outcome: ActionOutcome) in
-            XCTAssert(outcome == .success, "Action should have completed successfully")
-            completionExpectation.fulfill()
-        })
-
-        waitForExpectations(timeout: 5, handler: nil)
-        
-        XCTAssert(presenter.called, "Action was not performed")
-    }
-
-    /// Verify that calling an action that must be called on the main queue and completes asynchronously does so when called
-    /// from the a background queue.
-    func testMainQueueAsyncCompletingActionFromBackgroundQueue() {
-        // Use the presenter to detect if the body of the action was actually called, to guard against the session
-        // giving us completion without invoking the action
-        let presenter = MockPresenter()
-        
-        let completionExpectation = expectation(description: "Async completion called")
-        DispatchQueue.global(qos: .background).async {
-            ActionSession.main.perform(TestFeatures.asyncCompletingMainThreadAction,
-                                       input: .noInput,
-                                       presenter: presenter,
-                                       completion: { (outcome: ActionOutcome) in
-                XCTAssert(outcome == .success, "Action should have completed successfully")
-                completionExpectation.fulfill()
-            })
+        if let queueToUse = queue {
+            queueToUse.async(execute: block)
+        } else {
+            block()
         }
         
-        waitForExpectations(timeout: 5, handler: nil)
-        
-        XCTAssert(presenter.called, "Action was not performed")
-    }
-
-    /// Verify that calling an action that must be called on a non-main queue and completes asynchronously does so when called
-    /// from the main queue.
-    func testBackgroundQueueAsyncCompletingActionFromMainQueue() {
-        // Use the presenter to detect if the body of the action was actually called, to guard against the session
-        // giving us completion without invoking the action
-        let presenter = MockPresenter()
-        
-        let completionExpectation = expectation(description: "Async completion called")
-        Sessions.backgroundSession.perform(TestFeatures.asyncCompletingBackgroundThreadAction,
-                                           input: .noInput,
-                                           presenter: presenter,
-                                           completion: { (outcome: ActionOutcome) in
-            XCTAssert(outcome == .success, "Action should have completed successfully")
-            completionExpectation.fulfill()
-        })
-
         waitForExpectations(timeout: 5, handler: nil)
         
         XCTAssert(presenter.called, "Action was not performed")
     }
     
+    /// Verify that calling an action that must be called on the main queue, completes when called
+    /// from the main queue.
+    func testMainQueueSyncActionFromMainQueue() {
+        _testAction(TestFeatures.syncMainThreadAction,
+                    session: ActionSession.main,
+                    asyncOnQueue: .main)
+    }
+
+    /// Test that calling a main-queue action asynchronously on another queue works.
+    func testMainQueueSyncActionFromNonMainQueue() {
+        _testAction(TestFeatures.syncMainThreadAction,
+                    session: ActionSession.main,
+                    asyncOnQueue: DispatchQueue.global(qos: .background))
+    }
+
+    /// Verify that calling an action that must be called on a background queue, completes when called
+    /// from the main queue.
+    func testBackgroundQueueSyncActionFromMainQueue() {
+        _testAction(TestFeatures.syncBackgroundThreadAction,
+                    session: Sessions.backgroundSession,
+                    asyncOnQueue: nil)
+    }
+
+    /// Verify that calling an action that must be called on a background queue, completes when called
+    /// from the correct background queue.
+    func testBackgroundQueueSyncActionFromBackgroundQueue() {
+        _testAction(TestFeatures.syncBackgroundThreadAction,
+                    session: Sessions.backgroundSession,
+                    asyncOnQueue: SyncBackgroundThreadAction.queue)
+    }
+ 
+    /// Verify that calling an action that must be called on the main queue and completes asynchronously does so when called
+    /// from the main queue.
+    func testMainQueueAsyncCompletingActionFromMainQueue() {
+        _testAction(TestFeatures.asyncCompletingMainThreadAction,
+                    session: .main,
+                    asyncOnQueue: nil)
+    }
+
+    /// Verify that calling an action that must be called on the main queue and completes asynchronously does so when called
+    /// from the a background queue.
+    func testMainQueueAsyncCompletingActionFromBackgroundQueue() {
+        _testAction(TestFeatures.asyncCompletingMainThreadAction,
+                    session: .main,
+                    asyncOnQueue: .global(qos: .background))
+    }
+
+    /// Verify that calling an action that must be called on a non-main queue and completes asynchronously does so when called
+    /// from the main queue.
+    func testBackgroundQueueAsyncCompletingActionFromMainQueue() {
+        _testAction(TestFeatures.asyncCompletingBackgroundThreadAction,
+                    session: Sessions.backgroundSession,
+                    asyncOnQueue: nil)
+    }
+    
     /// Verify that calling an action that must be called on a non-main queue and completes asynchronously does so when called
     /// from the background queue.
     func testBackgroundQueueAsyncCompletingActionFromBackgroundQueue() {
-        // Use the presenter to detect if the body of the action was actually called, to guard against the session
-        // giving us completion without invoking the action
-        let presenter = MockPresenter()
-        
-        let completionExpectation = expectation(description: "Async completion called")
-        SyncBackgroundThreadAction.queue.async {
-            Sessions.backgroundSession.perform(TestFeatures.asyncCompletingBackgroundThreadAction,
-                                               input: .noInput,
-                                               presenter: presenter,
-                                               completion: { (outcome: ActionOutcome) in
-                XCTAssert(outcome == .success, "Action should have completed successfully")
-                completionExpectation.fulfill()
-            })
-        }
-        waitForExpectations(timeout: 5, handler: nil)
-        
-        XCTAssert(presenter.called, "Action was not performed")
+        _testAction(TestFeatures.asyncCompletingBackgroundThreadAction,
+                    session: Sessions.backgroundSession,
+                    asyncOnQueue: SyncBackgroundThreadAction.queue)
     }
 }
 
